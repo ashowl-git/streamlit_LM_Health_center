a7be1b48be47c0cbaca6884662eaa7126c3cc82b
ashowl-git <ashaahn@gmail.com>
2023-03-08 15:14:25 +0900

    e

--- pages/streamlit_LM_ECO2OD_Health_center.py
+++ pages/streamlit_LM_ECO2OD_Health_center.py
@@ -68,7 +68,7 @@ from sklearn.metrics import mean_squared_log_error
 #     "Main Page": main_page,
 #     "Page 2": page2,
 #     "Page 3": page3,
-}
+# }
 
 # selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 # page_names_to_funcs[selected_page]()
